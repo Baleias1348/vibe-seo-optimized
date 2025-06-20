6b9e7598220f796b94a24f2681fc8a16b89a3e9a
Baleias1348 <baleias1348@gmail.com>
2025-06-20 01:46:58 -0300

    w
    w

---
+++ vite.netlify.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  server: {
+    port: 8081,
+    open: true
+  }
+});
